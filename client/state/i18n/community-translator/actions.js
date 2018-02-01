3569636427d0359dcb1b4c902762429518782b50
ramonjd <ramonjd@gmail.com>
2018-02-01 19:00:55 +1100

    moving the ct over to it's own React component with util libs

--- client/state/i18n/community-translator/actions.js
+++ client/state/i18n/community-translator/actions.js
@@ -6,17 +6,7 @@
 import {
 	I18N_COMMUNITY_TRANSLATOR_ACTIVATE,
 	I18N_COMMUNITY_TRANSLATOR_DEACTIVATE,
-	I18N_COMMUNITY_TRANSLATOR_REFRESH,
 } from 'state/action-types';
-
-/**
- * Action creator function: I18N_COMMUNITY_TRANSLATOR_REFRESH
- *
- * @return {Object} action object
- */
-export const refreshCommunityTranslator = () => ( {
-	type: I18N_COMMUNITY_TRANSLATOR_REFRESH,
-} );
 
 /**
  * Action creator function: I18N_COMMUNITY_TRANSLATOR_ACTIVATE
@@ -35,4 +25,3 @@ export const activateCommunityTranslator = () => ( {
 export const deactivateCommunityTranslator = () => ( {
 	type: I18N_COMMUNITY_TRANSLATOR_DEACTIVATE,
 } );
-
--- client/state/i18n/community-translator/test/actions.js
+++ client/state/i18n/community-translator/test/actions.js
@@ -4,7 +4,7 @@
  * Internal dependencies
  */
 import {
-	refreshCommunityTranslator,
+	,
 	activateCommunityTranslator,
 	deactivateCommunityTranslator
 } from '../actions';
@@ -12,16 +12,9 @@ import {
 import {
 	I18N_COMMUNITY_TRANSLATOR_ACTIVATE,
 	I18N_COMMUNITY_TRANSLATOR_DEACTIVATE,
-	I18N_COMMUNITY_TRANSLATOR_REFRESH,
 } from 'state/action-types';
 
 describe( 'community translator actions', () => {
-	test( '#refreshCommunityTranslator()', () => {
-		expect( refreshCommunityTranslator() ).toEqual( {
-			type: I18N_COMMUNITY_TRANSLATOR_REFRESH,
-		} );
-	} );
-
 	test( '#activateCommunityTranslator()', () => {
 		expect( activateCommunityTranslator() ).toEqual( {
 			type: I18N_COMMUNITY_TRANSLATOR_ACTIVATE,
